f69f270a82db8f26fdec8946136cdc178300c771
NekodaMushi <study.fabien@gmail.com>
2024-03-19 09:23:17 +0100

    config proxy

--- property-pulse/app/properties/page.jsx
+++ property-pulse/app/properties/page.jsx
@@ -1,7 +1,20 @@
 import PropertyCard from "@/components/PropertyCard";
-import properties from "@/properties.json";
+// import properties from "@/properties.json";
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
 
-function page() {
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function PropertiesPage() {
+  const properties = await fetchProperties();
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
@@ -19,4 +32,4 @@ function page() {
   );
 }
 
-export default page;
+export default PropertiesPage;
