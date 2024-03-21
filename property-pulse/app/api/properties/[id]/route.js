b74d211b3ac8478e026be15b48146aeb23173223
NekodaMushi <study.fabien@gmail.com>
2024-03-21 12:37:00 +0100

    single property

--- property-pulse/app/api/properties/[id]/route.js
+++ property-pulse/app/api/properties/[id]/route.js
@@ -4,9 +4,10 @@ import Property from "@/models/Property";
 export const GET = async (request, { params }) => {
   try {
     await connectDB();
+
     const property = await Property.findById(params.id);
 
-    if (!property) return new Response("Property not found", { status: 404 });
+    if (!property) return new Response("Property Not Found", { status: 404 });
 
     return Response.json(property);
   } catch (error) {
@@ -14,3 +15,17 @@ export const GET = async (request, { params }) => {
     return new Response("Something went bad", { status: 500 });
   }
 };
+
+// export const GET = async (request, { params }) => {
+//   try {
+//     await connectDB();
+//     const property = await Property.findById(params.id);
+
+//     if (!property) return new Response("Property not found", { status: 404 });
+
+//     return Response.json(property);
+//   } catch (error) {
+//     console.log(error);
+//     return new Response("Something went bad", { status: 500 });
+//   }
+// };
--- property-pulse/app/properties/[id]/page.jsx
+++ property-pulse/app/properties/[id]/page.jsx
@@ -3,32 +3,47 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { fetchProperty } from "@/utils/requests";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 
 const PropertyPage = () => {
   const { id } = useParams();
 
   const [property, setProperty] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchPropertyData = async () => {
-      if (!id) return;
       try {
+        if (!id) return;
         const property = await fetchProperty(id);
+
         setProperty(property);
       } catch (error) {
         console.error("Error fetching property", error);
       } finally {
-        setLoading(false);
+        setIsLoading(false);
       }
     };
-
-    if (property === null) {
-      fetchPropertyData();
-    }
+    if (property === null) fetchPropertyData();
   }, [id, property]);
 
-  return <div>PropertyPage</div>;
+  if (!property && !isLoading) {
+    return (
+      <h1 className="text-center text-2xl font-bold mt-10">
+        Property Not Found
+      </h1>
+    );
+  }
+
+  return (
+    <>
+      {!isLoading && property && (
+        <>
+          <PropertyHeaderImage image={property.images[0]} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default PropertyPage;
